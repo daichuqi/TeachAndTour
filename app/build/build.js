da780f361369047b436b21e656a957b932c30a0b
chuqidai <daichuqi@gmail.com>
2015-09-28 18:13:59 -0400

    cover pics

--- app/build/build.js
+++ app/build/build.js
@@ -229,7 +229,7 @@ var _react2 = _interopRequireDefault(_react);
 
 var _reactBootstrap = require('react-bootstrap');
 
-var img = ['../assets/images/bg2.jpeg'];
+var img = ['../assets/images/bg0.jpg', '../assets/images/bg2.jpg', '../assets/images/bg3.jpg', '../assets/images/bg4.jpg'];
 
 var ControlledCarousel = (function (_React$Component) {
   _inherits(ControlledCarousel, _React$Component);
@@ -262,6 +262,81 @@ var ControlledCarousel = (function (_React$Component) {
           _reactBootstrap.CarouselItem,
           null,
           _react2['default'].createElement('img', { className: 'bannerImg', src: img[0] }),
+          _react2['default'].createElement(
+            'div',
+            { className: 'carousel-caption' },
+            _react2['default'].createElement(
+              'div',
+              { className: 'carousel-title' },
+              'Explore China by teaching English'
+            ),
+            _react2['default'].createElement(
+              'span',
+              { className: 'imageLocation' },
+              _react2['default'].createElement('i', { className: 'fa fa-map-marker' }),
+              ' Shanghai China'
+            ),
+            _react2['default'].createElement(
+              'p',
+              null,
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            )
+          )
+        ),
+        _react2['default'].createElement(
+          _reactBootstrap.CarouselItem,
+          null,
+          _react2['default'].createElement('img', { className: 'bannerImg', src: img[1] }),
+          _react2['default'].createElement(
+            'div',
+            { className: 'carousel-caption' },
+            _react2['default'].createElement(
+              'div',
+              { className: 'carousel-title' },
+              'Explore China by teaching English'
+            ),
+            _react2['default'].createElement(
+              'span',
+              { className: 'imageLocation' },
+              _react2['default'].createElement('i', { className: 'fa fa-map-marker' }),
+              ' Shanghai China'
+            ),
+            _react2['default'].createElement(
+              'p',
+              null,
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            )
+          )
+        ),
+        _react2['default'].createElement(
+          _reactBootstrap.CarouselItem,
+          null,
+          _react2['default'].createElement('img', { className: 'bannerImg', src: img[2] }),
+          _react2['default'].createElement(
+            'div',
+            { className: 'carousel-caption' },
+            _react2['default'].createElement(
+              'div',
+              { className: 'carousel-title' },
+              'Explore China by teaching English'
+            ),
+            _react2['default'].createElement(
+              'span',
+              { className: 'imageLocation' },
+              _react2['default'].createElement('i', { className: 'fa fa-map-marker' }),
+              ' Shanghai China'
+            ),
+            _react2['default'].createElement(
+              'p',
+              null,
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            )
+          )
+        ),
+        _react2['default'].createElement(
+          _reactBootstrap.CarouselItem,
+          null,
+          _react2['default'].createElement('img', { className: 'bannerImg', src: img[3] }),
           _react2['default'].createElement(
             'div',
             { className: 'carousel-caption' },
--- app/js/components/carousel.js
+++ app/js/components/carousel.js
@@ -3,7 +3,11 @@ import React from 'react';
 import {Carousel,CarouselItem} from 'react-bootstrap';
 
 
-var img = ['../assets/images/bg2.jpeg'];
+var img =
+['../assets/images/bg0.jpg',
+'../assets/images/bg2.jpg',
+'../assets/images/bg3.jpg',
+'../assets/images/bg4.jpg'];
 
 class ControlledCarousel extends React.Component{
   constructor(){
@@ -26,6 +30,36 @@ class ControlledCarousel extends React.Component{
       <Carousel activeIndex={this.state.index} direction={this.state.direction} onSelect={this.handleSelect}>
         <CarouselItem>
             <img className="bannerImg" src={img[0]}/>
+            <div className='carousel-caption'>
+              <div className="carousel-title">Explore China by teaching English</div>
+              <span className="imageLocation">
+                <i className="fa fa-map-marker"></i> Shanghai China
+              </span>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </CarouselItem>
+                <CarouselItem>
+            <img className="bannerImg" src={img[1]}/>
+            <div className='carousel-caption'>
+              <div className="carousel-title">Explore China by teaching English</div>
+              <span className="imageLocation">
+                <i className="fa fa-map-marker"></i> Shanghai China
+              </span>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </CarouselItem>
+        <CarouselItem>
+            <img className="bannerImg" src={img[2]}/>
+            <div className='carousel-caption'>
+              <div className="carousel-title">Explore China by teaching English</div>
+              <span className="imageLocation">
+                <i className="fa fa-map-marker"></i> Shanghai China
+              </span>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </CarouselItem>
+                <CarouselItem>
+            <img className="bannerImg" src={img[3]}/>
             <div className='carousel-caption'>
               <div className="carousel-title">Explore China by teaching English</div>
               <span className="imageLocation">
